94290aebda3fc905d6cb26b982d13eac38bb0629
Ojames <ojames0044@gmail.com>
2025-07-11 22:44:45 +0100

    Dropwdown menu updated with new styles and functionality

--- social platform.js
+++ social platform.js
@@ -1,3 +1,4 @@
+
 document.getElementsByClassName("compose").addEventListener("submit", function(event) {
   event.preventDefault();
   let confirmSubmit = confirm("Are you sure you want want to post?");
@@ -6,3 +7,18 @@ document.getElementsByClassName("compose").addEventListener("submit", function(e
   }
 }
 )
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
