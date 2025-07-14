23f182a50c34aa07d8ac4ee22aa665ee6e94da70
Ojames <ojames0044@gmail.com>
2025-07-14 20:50:10 +0100

    Dropdown list updated

--- social platform.js
+++ social platform.js
@@ -6,3 +6,22 @@ document.getElementsByClassName("compose").addEventListener("submit", function(e
   }
 }
 )
+
+// Function to toggle the dropdown menu
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+}
